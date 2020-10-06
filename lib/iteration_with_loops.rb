5afa817a0d57546096556f7a1625fd83b5bdad3d
Javaria Brascom <javariab17@gmail.com>
2020-10-06 20:58:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ while row_index<src.count do
   element_index=0 
   minimum_value=''
   while element_index< src[row_index].count do
-  if src[row_index][element_index].length < minimum_value.length
+  if src[row_index][element_index].length < minimum_value.length 
     minimum_value= src[row_index][element_index]
   end
   element_index+=1
