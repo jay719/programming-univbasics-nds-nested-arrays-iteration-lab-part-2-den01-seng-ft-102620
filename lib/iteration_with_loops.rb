3c2d69520509c4a67d5f36381a5cf5835e38f74b
Javaria Brascom <javariab17@gmail.com>
2020-10-06 20:57:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,10 @@ def find_min_in_nested_arrays(src)
 row_index=0 
 while row_index<src.count do
   element_index=0 
-  while element_index< src[row_index].count 
-  minimum_value= src[row_index][element_index].min
+  minimum_value=''
+  while element_index< src[row_index].count do
+  if src[row_index][element_index].length< minimum_value.length
+    minimum_value= src[row_index][element_index]
   end
   element_index+=1
 end
